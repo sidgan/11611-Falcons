10f07d0d5235dde2999b775683e236ee9b0d7124
Deepak Gopinath <dgopina1@andrew.cmu.edu>
2016-04-12 19:31:57 -0400

    Bug fixes

--- generation/gen.py
+++ generation/gen.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 from nltk.parse import stanford
 from nltk.tag import StanfordNERTagger
 from nltk.tree import Tree
@@ -6,12 +8,16 @@ from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
 import calendar
 
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
 PROJECT_HOME='/home/deepak/Downloads/NLP/project'
 PARSER_PATH=os.path.join(PROJECT_HOME, 'stanford-parser-full-2015-04-20')
 NER_PATH=os.path.join(PROJECT_HOME, 'stanford-ner-2015-04-20')
 PARSER_MODEL_PATH=os.path.join(PARSER_PATH, 'stanford-parser-3.5.2-models/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz')
 NER_MODEL_PATH=os.path.join(NER_PATH, 'classifiers/english.all.3class.distsim.crf.ser.gz')
-DELIMITERS=" "
+
 WHO_PRONOUNS=set(["i", "he", "she", "you", "we", "they"])
 WHAT_PRONOUNS=set(["it", "this", "that"])
 VERB_PAST='VBD'
@@ -20,16 +26,20 @@ VERB_FUTURE='VB'
 DAYS_MONTHS=[name.lower() for name in calendar.day_name[:] + calendar.month_name[1:] + ["a.m", "p.m"]]
 LOCATION_TIME_PP=["in", "on", "over", "at"]
 
+with open("sample.txt") as sample_file:
+    raw_text = sample_file.readline()
+
 os.environ['STANFORD_PARSER'] = PARSER_PATH
 os.environ['STANFORD_MODELS'] = PARSER_PATH
 os.environ['CLASSPATH'] = PARSER_PATH + ':' + NER_PATH
 
 s = ["The boy is playing with a ball.", "Sachin won an award.", "I was born on a Tuesday", "Manchester United played on a Monday in Pittsburgh", "Why Did You Put Up That Banner When We Won 4 Nil?"]
+s = raw_text.split(".")[:]
 parser = stanford.StanfordParser(model_path=PARSER_MODEL_PATH)
 ner_tagger = StanfordNERTagger(NER_MODEL_PATH)
 lemmatizer = WordNetLemmatizer()
 
-sentences = parser.raw_parse_sents((s[:]))
+sentences = parser.raw_parse_sents(([x.lower() for x in s[:]]))
 #tags = [ner_tagger.tag(sentence.split(DELIMITERS)) for sentence in s]
 
 def get_wh_word(np_tree):
@@ -42,10 +52,10 @@ def get_wh_word(np_tree):
             return "What"
     return "What"
 
-
-def apply_naive_rule(sentence):
+def apply_subject_rule(sentence):
     s = sentence[0]
     if s[0].label() == "NP" and s[1].label() == "VP":
+        print "Subject"
         print get_wh_word(s[0]) + " " +  " ".join(s[1].flatten())
 
 
@@ -53,7 +63,7 @@ def check_location(tree, count, curr_count = 0):
     for child in tree:
         if type(child) == Tree:
             for grandchild in child:
-                if type(grandchild) == Tree and grandchild.label() == "PP" and grandchild[0,0].lower() in LOCATION_TIME_PP:
+                if type(grandchild) == Tree and grandchild.label() == "PP" and grandchild[0,0] in LOCATION_TIME_PP:
                     curr_count += 1
                     if curr_count > count:
                         return grandchild
@@ -72,11 +82,9 @@ def get_verb_form(label):
         verb = "does"
     return verb
 
-
-
 def get_tense_root(tree):
     verb = get_verb_form(tree[0].label())
-    verb_root = lemmatizer.lemmatize(tree[0].leaves()[0], wn.VERB)
+    verb_root = lemmatizer.lemmatize(tree[0].leaves()[0], u'v')
     tree.remove(tree[0])
     tree.insert(0, Tree('Inserted', [verb_root]))
     return verb
@@ -84,11 +92,9 @@ def get_tense_root(tree):
 
 def get_wh_word_location_time(location_tree):
     for word in location_tree.leaves():
-        if word.lower() in DAYS_MONTHS:
-            return "when"
-    return "where"
-
-
+        if word in DAYS_MONTHS or re.match(r'[1-2][0-9][0-9][0-9]\b', word):
+            return "When"
+    return "Where"
 
 def remove_location(tree, location_tree):
     if tree.count(location_tree) > 0:
@@ -103,7 +109,7 @@ def remove_location(tree, location_tree):
         return None, None
 
 def apply_location_rule(orig_sentence):
-    sentence = orig_sentence.copy()
+    sentence = orig_sentence.copy(deep=True)
     location_tree = check_location(sentence, 0)
     count = 1
     while location_tree != None:
@@ -111,10 +117,11 @@ def apply_location_rule(orig_sentence):
         print_sent = [wh_word, verb] + sentence.leaves()
         print " ".join(print_sent)
         count += 1
-        sentence = orig_sentence.copy()
+        sentence = orig_sentence.copy(deep=True)
         location_tree = check_location(sentence, count)
 
 for line in sentences:
     for sentence in line:
+        apply_subject_rule(sentence)
         apply_location_rule(sentence)
-        apply_naive_rule(sentence)
+        sentence.draw()
