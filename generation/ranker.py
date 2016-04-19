0b06f1b3768232ac69c9b21930c2860c298b68b3
Sanchit Agarwal <sanchit.ag89@gmail.com>
2016-04-18 23:02:59 -0400

    ranker modifications

--- generation/ranker.py
+++ generation/ranker.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from _collections import defaultdict
 
-def get_score(question, q_type):
+def get_score(question,q_type):
     
     payload = {'key': 'deepakkey', 'data': question}
     
@@ -42,14 +42,10 @@ def get_score(question, q_type):
     #print length
     final_score = 1*length + 10 - (5*no_of_errors)
     
-    if q_type == 'yes':
-        final_score+=10
-    elif q_type =='no':
-        final_score-=5
-    
     return final_score
     
 def compare(t):
+    
     return t[1]
 
 def rank(question_type_list,N):
@@ -57,22 +53,28 @@ def rank(question_type_list,N):
     type_to_questions = defaultdict(list)
     
     for pair in question_type_list:
+        
         question = pair[0]
         question_type = pair[1]
+        
         score = get_score(question,question_type)
+        
         type_to_questions[question_type].append(tuple([question,score]))
-
+    
     q_types = type_to_questions.keys()
     
     for q_type in q_types:
+        
         q_list = type_to_questions[q_type]
+        
         type_to_questions[q_type] = sorted(q_list,reverse=True,key=compare)
+        
         
     #create a new merged list mixing questions of different types
     ranked_list = []
     
-    yesQ = type_to_questions['yes']
-    t1 = len(yesQ)
+    yes_noQ = type_to_questions['yes_no']
+    t1 = len(yes_noQ)
     
     whatQ = type_to_questions['what']
     t2 = len(whatQ)
@@ -80,11 +82,8 @@ def rank(question_type_list,N):
     whoQ = type_to_questions['who']
     t3 = len(whoQ)
     
-    noQ = type_to_questions['no']
-    t4 = len(noQ)
     
-    
-    a1 = int(N*.5)
+    a1 = int(N*.6)
     if t1<a1:
         a1 = t1
     t1-=a1
@@ -94,46 +93,38 @@ def rank(question_type_list,N):
         a2 = t2
     t2-=a2
     
-    a3 = int(N*.2)
+    a3 = N-a1-a2
     if t3<a3:
         a3 = t3
     t3-=a3
         
-    a4 = N-a1-a2-a3
-    if t4<a4:
-        a4 = t4
-    t4-=a4
-        
-    total = a1+a2+a3+a4
+    total = a1+a2+a3
     
     for i in range(a1):
-        if i<len(yesQ):
-            ranked_list.append(yesQ[i][0])    
+        ranked_list.append(yes_noQ[i][0])    
     for i in range(a2):
         ranked_list.append(whatQ[i][0])
     for i in range(a3):
         ranked_list.append(whoQ[i][0])
-    for i in range(a4):
-        ranked_list.append(noQ[i][0])
-        
+
     while(total<N):
         if t1>0:
-            ranked_list.append(yesQ[a1][0])
+            ranked_list.append(yes_noQ[a1][0])
             a1+=1
             t1-=1
         total+=1
     
     return ranked_list
-
-def main():
-    questionList = [('Does most people learn English for practical rather than ideological reasons?','yes'),
-                    ('Did decolonisation proceed throughout the British Empire in the 1950s and 1960s?','yes'),
-                    ('Does english continue to be an official language of India?','yes'),
+ 
+def main():       
+    questionList = [('Does most people learn English for practical rather than ideological reasons?','yes_no'),
+                    ('Did decolonisation proceed throughout the British Empire in the 1950s and 1960s?','yes_no'),
+                    ('Does english continue to be an official language of India?','yes_no'),
                     ('Is also?','yes'),
-                    ('Is newspaper publishing book publishing?','yes'),
-                    ('Is english an official language in most countries?','no')]
+                    ('Is newspaper publishing book publishing?','yes_no'),
+                    ('Is english an official language in most countries?','yes_no'),
+                    ('What is an official language of India?','what')]
 
     print rank(questionList,3)
-    #question = 'Does most people learn English for practical rather than ideological reasons?'
-    #print get_score(question)
+
 if __name__=="__main__":main()
